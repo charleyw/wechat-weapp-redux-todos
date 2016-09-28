db5d5442d53369f824e6faa00795ae31db1ec06c
Chao Wang <cwang8023@gmail.com>
2016-09-28 16:07:21 +0800

    make todo works

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,29 @@
 //index.js
 //获取应用实例
 var app = getApp()
+var todos = [
+      {id: 0, text: '看小程序文档', completed: false},
+      {id: 1, text: '看小程序设计指南', completed: false},
+      {id: 2, text: '写小程序版本的Todo', completed: false},
+      {id: 3, text: '使用Redux', completed: false}
+    ];
+
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    todos: todos 
   },
-  //事件处理函数
-  bindViewTap: function() {
-    console.log(app.store)
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  addTodo: function (e) {
+    todos.push({id: todos.length, text: e.detail.value.todo, completed: false});
+    this.setData({todos})
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+  handleCheck: function (e) {
+    const id = e.target.id
+    this.setData({todos: this.data.todos.map(t => {
+      if (t.id.toString() !== id){
+        return t
+      }
+
+      return Object.assign({}, t, {completed: !t.completed})
+    })})
   }
 })
