8d601b5f67b89a7244fb36145b0586f283e7aaec
Chao Wang <cwang8023@gmail.com>
2016-10-09 16:29:22 +0800

    enable devtools

--- configureStore.js
+++ configureStore.js
@@ -2,16 +2,15 @@ const {createStore, compose} = require('./libs/redux.js');
 const devTools = require('./libs/remote-redux-devtools.js').default;
 const reducer = require('./reducers/index.js')
 
-function configureStore() {
-  return createStore(reducer);
-}
 // function configureStore() {
-//   return createStore(reducer, compose(devTools({
-//     hostname: 'localhost',
-//     port: 5678,
-//     secure: false,
-//     realtime: true
-//   })));
+//   return createStore(reducer);
 // }
+function configureStore() {
+  return createStore(reducer, compose(devTools({
+    hostname: 'localhost',
+    port: 5678,
+    secure: false
+  })));
+}
 
 module.exports = configureStore;
