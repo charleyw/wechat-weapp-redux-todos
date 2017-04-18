19770a668739bb91c41d96db1d8489d5e32cd559
Chao Wang <cwang8023@gmail.com>
2017-04-18 11:05:58 +0800

    update fetch js to handle http request error

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,3 +1,9 @@
+const preDefinedError = {
+  404: '请求地址不存在',
+  500: '服务器发生错误',
+  401: '用户未认证'
+};
+
 export const fetchJson = (url, options = {}) => {
   const requestHeaders = options.header || {Accept: 'application/json'};
 
@@ -10,7 +16,12 @@ export const fetchJson = (url, options = {}) => {
       url,
       ...options,
       header: requestHeaders,
-      success: resolve,
+      success: function ({data, statusCode}) {
+        if (statusCode < 200 || statusCode >= 300) {
+          reject(new Error((data && data.message) || preDefinedError[statusCode] || '请求失败'));
+        }
+        resolve({data, statusCode});
+      },
       fail: reject
     })
   })
